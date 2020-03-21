f5f4468bdd01335ff1ec23d66ca03806acaea386
Vitali <terrmit574@gmail.com>
2020-03-21 23:32:38 +0300

    feat: solution expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,9 +4,41 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const splitExpr = expr.split('');
+    checkBrecketsPair(splitExpr);
+    checkDivisionByZero(splitExpr);
+
+    const solidString = expr.replace(/\s/g, '');
+    return new Function('return ' + solidString)();
+}
+
+function checkBrecketsPair(splitExpr) {
+    let counterBreckets = 0;
+
+    for (let value of splitExpr) {
+        if (value === "(") {
+            counterBreckets++;
+        }
+        if (value === ")") {
+            counterBreckets--;
+        }
+    }
+
+    if (counterBreckets !== 0)  {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+}
+
+function checkDivisionByZero(splitExpr) {
+    for (let i = 0; i < splitExpr.length; i++) {
+        if (splitExpr[i] === "/") {
+            if (splitExpr[i + 2] === "0") {
+                throw new Error("TypeError: Division by zero.");
+            }
+        }
+    }
 }
 
 module.exports = {
     expressionCalculator
-}
+};
